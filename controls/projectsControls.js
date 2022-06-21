430ea7f93cc6c23e345f8d8f91f4a12132339e34
kennedy7 <kennedyhillary6@gmail.com>
2022-06-21 21:28:09 +0100

    fourth commit

--- controls/projectsControls.js
+++ controls/projectsControls.js
@@ -40,8 +40,27 @@ exports.GetStudentProject = function (req, res) {
         });
       });
   } 
+
+  exports.GetProjectList = function (req, res){
+      pool.query(
+          `SELECT * from projects`,
+          (err, results) => {
+              if (req.user.role === 'Admin') {
+                  return res.render('ProjectsView', {
+                      user: req.user.name,
+                      projects: results.rows
+                  })
+              }
+              res.render('ProjectsView', {
+                  user: req.user.name,
+                  projects: results.rows.filter(result => result.userid === req.user.id)
+              })
+          }
+      )
+  }
   
   exports.UpdateProject = async function (req, res){
+
     await  pool.query(
         'UPDATE projects set department=$1, level =$2, title =$3, content = $4, summary= $5 where id =$6',
         [ req.body.department, 
@@ -53,8 +72,13 @@ exports.GetStudentProject = function (req, res) {
           (err, results) => {
          return res.redirect('/users/projectlist');
         });
-    
   }
 
- 
-
+  exports.DeleteProject = function (req, res){
+    var id = req.params.id;
+    pool.query(`DELETE from projects where id = $1`,
+      [id],
+      (err, results) => {
+        res.redirect('/users/projectlist')
+      })
+  }
--- routes/projectsRoute.js
+++ routes/projectsRoute.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const projectRouter = express.Router()
-const {
-    AddProject, GetStudentProject, EditProject, UpdateProject
-} = require('../controls/projectsControls')
-
-const {
-    authUser
-} = require('../basicAuth')
-const {
-    canViewProject
-} = require('../permissions/projects')
-const {
-    pool
-} = require('../dbConfig')
+const { AddProject, GetStudentProject, EditProject, UpdateProject, GetProjectList, DeleteProject} = require('../controls/projectsControls')
+const { authUser} = require('../basicAuth')
+const {canViewProject} = require('../permissions/projects')
+const {pool} = require('../dbConfig')
 
 projectRouter.get('/users/newproject', authUser, (req, res) => {
     res.render('Newproject', {
@@ -21,24 +12,7 @@ projectRouter.get('/users/newproject', authUser, (req, res) => {
 })
 projectRouter.post('/users/newproject', authUser, AddProject)
 
-projectRouter.get('/users/projectlist', authUser, (req, res) => {
-    pool.query(
-        `SELECT * from projects`,
-        (err, results) => {
-            if (req.user.role === 'Admin') {
-                return res.render('ProjectsView', {
-                    user: req.user.name,
-                    projects: results.rows
-                })
-            }
-            res.render('ProjectsView', {
-                user: req.user.name,
-                projects: results.rows.filter(result => result.userid === req.user.id)
-            })
-        }
-    )
-
-})
+projectRouter.get('/users/projectlist', authUser, GetProjectList)
 
 projectRouter.get('/users/projects/:id', authUser, GetStudentProject)
 
@@ -46,15 +20,7 @@ projectRouter.get('/edit/:id', authUser, EditProject);
 
 projectRouter.post('/update/:id', authUser, UpdateProject);
 
-projectRouter.delete('/delete/:id', (req, res) => {
-    var id = req.params.id;
-    pool.query(`DELETE from projects where id = $1`,
-      [id],
-      (err, results) => {
-        res.redirect('/users/projectlist')
-      })
-  
-  })
+projectRouter.delete('/delete/:id', DeleteProject)
 
 function authGetProject(req, res, next) {
     if (!canViewProject(req.user, req.project)) {
